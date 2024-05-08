a00f1d8f32620bec2add12cb7cf9147c8867053c
jklama <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>
2024-05-08 22:12:24 +0545

    added mutations and it all works

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(
-    process.env.MONGO_URI ||
-      "mongodb+srv://nimatmmg3:<password>@banking.m83kjiq.mongodb.net/?retryWrites=true&w=majority&appName=Banking"
-  );
+  const conn = await mongoose.connect(process.env.MONGO_URI);
 
   console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
 };
